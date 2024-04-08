128323d61e1fd7f45ec85dc7589e0c2221aaac9f
ANIRUDH PRATAP SINGH <rohanth9990@gmail.com>
2024-04-08 13:04:10 +0530

    Update

--- config/urls.py
+++ config/urls.py
@@ -17,20 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from shop.views import contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('page' , TemplateView.as_view(template_name='page1.html'), name='page1'),
-    path('about' , TemplateView.as_view(template_name='about.html'), name='about'),
-    path('pricing' , TemplateView.as_view(template_name='pricing.html'), name='pricing'),
-
+    path('',TemplateView.as_view(template_name='index.html'), name='home'),
+    path('page',TemplateView.as_view(template_name='page1.html'), name='page1'),
+    path('about',TemplateView.as_view(template_name='about.html'),name='about'),
+    path('pricing',TemplateView.as_view(template_name='pricing.html'),name='pricing'),
+    path('contact',contact_view, name='contact'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, documents_roots=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, documents_roots=settings.STATIC_ROOT)
-
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here. 
+# Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
@@ -9,13 +9,13 @@ class Category(models.Model):
     def __str__(self):
         return self.title
     
-    class contact(models.Model):
-        name = models.CharField(max_length=30)
-        email = models.EmailField()
-        subject = models.CharField(max_length=50)
-        message = models.TextField()
-        created_at = models.DateTimeField(auto_now_add= True, default=None)
 
+class Contact(models.Model):
+    name =models.CharField(max_length=30)
+    email = models.EmailField()
+    subject = models.CharField(max_length=50)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
